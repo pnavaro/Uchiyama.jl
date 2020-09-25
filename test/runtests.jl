e34226b8a0e8bffe0e24e542570fd48adcf54bd8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-09-25 11:20:35 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,51 +1,6 @@
-using Plots
-using Random
 using Test
+using Random
 
-include("../src/periodic_uchiyama.jl")
-
-#@testset "run" begin
-
-    n = 10
-    ϵ = 0.1
-    q = [[0.5       ,0.5       ],
-         [0.20006862,0.38139954],
-         [0.32267135,0.72687046],
-         [0.68044674,0.78095695],
-         [0.77473372,0.51989917],
-         [0.4949439 ,0.23201753],
-         [0.74204115,0.28248482],
-         [0.63101615,0.3650337 ],
-         [0.5846235 ,0.64552143],
-         [0.45656816,0.76644607]]
-
-    v = [[ 0, 1],
-         [ 1, 0],
-         [-1, 0],
-         [ 0,-1],
-         [ 1, 0],
-         [ 0, 1],
-         [ 0, 1],
-         [-1, 0],
-         [-1, 0],
-         [ 0,-1]]
-
-    collisions = PCollisionMatrix(n, q, v, ϵ)
-    step!(n, ϵ, q, v, collisions)
-
-#    anim = @animate for _ in 1:100
-#
-#        dt = step!(n, ϵ, q, v, collisions)
-#        p = plot(size=(500,500), xlims=(0,1), ylims=(0,1), 
-#        grid=false, axis=nothing, legend=false, framestyle = :box, widen = false)
-#        scatter!(getindex.(q,1), getindex.(q,2), markershape = :square, 
-#                 markersize=50, aspect_ratio=:equal)
-#
-#    end
-#
-#    gif(anim, joinpath(@__DIR__, "periodic_uchiyama.gif"), fps = 15)
-
-#end
 #=
 
 @testset "step" begin
@@ -67,44 +22,23 @@ include("../src/periodic_uchiyama.jl")
 
 
 end
+=#
+
+@testset "overlap" begin
+
+end
 
 
 @testset "init_particles" begin
  
-     n = 10
+     import Uchiyama: init_particles
+     n = 100
      ϵ = 1 / n
      c = trunc(Int, 1000 * 2ϵ)
      rng = MersenneTwister(1234)
      
      q, v = init_particles(n, ϵ, rng)
 
-     plot(size=(500,500), xlims=(0,1), ylims=(0,1), 
-          grid=false, axis=nothing, legend=false, framestyle = :box, widen = false)
-     scatter!(getindex.(q,1), getindex.(q,2), markershape = :square, markersize=c, aspect_ratio=:equal)
-     savefig("particles.png")
- 
 end
 
-@testset "tempscoll" begin
-
-   ϵ = 0.1 
-
-   for p in 0.2:0.1:0.8
-       x = [p, 0.2]; v = [0.0,  1.0]
-       y = [p, 0.8]; w = [0.0, -1.0]
-       @test tempscoll(x, y, v, w, ϵ) ≈ 0.2
-       x = [0.2, p]; v = [ 1.0, 0.0]
-       y = [0.8, p]; w = [-1.0, 0.0]
-       @test tempscoll(x, y, v, w, ϵ) ≈ 0.2
-   end
-
-   x = [0.8,0.8]; v = [-1.0,-1.0]
-   y = [0.2,0.2]; w = [+1.0,1.0]
-   @test tempscoll(x, y, v, w, ϵ) ≈ 0.5
-   x = [0.2,0.8]; v = [+1.0,-1.0]
-   y = [0.8,0.2]; w = [-1.0,1.0]
-   @test tempscoll(x, y, v, w, ϵ) ≈ 0.5
-
-end
-tempscoll(q[l] + offset[i], q[k], v[l], v[k], ϵ)
-=#
+include("test_tempscoll.jl")
