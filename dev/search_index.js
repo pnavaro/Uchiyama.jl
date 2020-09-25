db3b20f324865fe859226e9c367d1340ac144eb0
Documenter.jl <documenter@juliadocs.github.io>
2020-09-25 17:54:51 +0000

    build based on be04218

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Uchiyama","category":"page"},{"location":"#Uchiyama","page":"Home","title":"Uchiyama","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Uchiyama]","category":"page"}]
+}
